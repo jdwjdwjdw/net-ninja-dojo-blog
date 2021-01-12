9f70c2897357bbb7ebdfa6bc1d0ee5b676d347e8
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-01-11 16:53:14 -0800

    Add conditional loading message while waiting for blogs data

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,26 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
 
       useEffect(() => {
-          fetch('http://localhost:8000/blogs')
+          // setTimeout added only to demonstrate the increased delay that will realistically happen in production
+          setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json()
             })
             .then((data) => {
-                setBlogs(data)
+                setBlogs(data);
+                setIsPending(false)
             })
+          }, 1000)
       }, []);
 
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
      );
