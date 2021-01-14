e2b084181040ba0686ed44e52589ed404bdf0c42
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-01-13 18:02:35 -0800

    Handle fetch errors, output any errors in template

--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,34 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
       useEffect(() => {
           // setTimeout added only to demonstrate the increased delay that will realistically happen in production
           setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false)
-            })
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error('could not fetch the data for that resource')
+                    }
+                    return res.json()
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false)
+                    setError(null)
+                })
+                .catch(err => {
+                    setIsPending(false)
+                    setError(err.message)
+                })
           }, 1000)
       }, []);
 
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div>}
             { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
