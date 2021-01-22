14505219e5abf8fa8ac2064b4e84d28537136681
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-01-21 16:14:59 -0800

    Add redirect to home after new blog is submitted

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('yoshi')
     const [isPending, setIsPending] = useState(false)
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,8 +19,13 @@ const Create = () => {
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify(blog)
         }).then(() => {
-            setIsPending(false)
+            console.log('new blog added')
+            setIsPending(false);
+            // history.go(-1); just goes back one
+            history.push('/')
         })
+
+        
     }
 
     return ( 
